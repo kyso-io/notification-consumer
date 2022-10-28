299d136b14b7c0e1573743175542964c37705b0a
Fran Barrena <francisco@kyso.io>
2022-10-28 15:56:41 +0200

    feat: added comments notifications

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,5 +1,6 @@
 import { KysoCommentsCreateEvent, KysoCommentsDeleteEvent, KysoCommentsUpdateEvent, KysoEventEnum } from '@kyso-io/kyso-model'
-import { Controller, Inject } from '@nestjs/common'
+import { MailerService } from '@nestjs-modules/mailer'
+import { Controller, Inject, Logger } from '@nestjs/common'
 import { EventPattern } from '@nestjs/microservices'
 import { Db } from 'mongodb'
 import { Constants } from '../constants'
@@ -9,14 +10,102 @@ export class CommentsController {
     constructor(
         @Inject(Constants.DATABASE_CONNECTION)
         private db: Db,
+        private readonly mailerService: MailerService
     ) {}
 
     @EventPattern(KysoEventEnum.COMMENTS_CREATE)
-    async handleCommentsCreated(kysoCommentsCreateEvent: KysoCommentsCreateEvent) {}
+    async handleCommentsCreated(event: KysoCommentsCreateEvent) {
+        Logger.log(KysoEventEnum.COMMENTS_CREATE, CommentsController.name)
+        Logger.debug(event, CommentsController.name)
+
+        const { frontendUrl, organization, team, report, user, comment} = event;
+
+        const centralizedMails: boolean = organization?.options?.notifications?.centralized || false
+        const emails: string[] = organization?.options?.notifications?.emails || []
+        const to = centralizedMails && emails.length > 0 ? emails : user.email
+
+        this.mailerService
+            .sendMail({
+                to,
+                subject: `New comment in report ${report.title}`,
+                template: 'comment-new',
+                context: {
+                    frontendUrl,
+                    organization,
+                    team,
+                    report,
+                    comment
+                },
+            })
+            .then((messageInfo) => {
+                Logger.log(`New comment mail ${messageInfo.messageId} sent to ${Array.isArray(to) ? to.join(', ') : to}`, CommentsController.name)
+            })
+            .catch((err) => {
+                Logger.error(`An error occurred sending new comment mail to ${Array.isArray(to) ? to.join(', ') : to}`, err, CommentsController.name)
+            })
+    }
 
     @EventPattern(KysoEventEnum.COMMENTS_UPDATE)
-    async handleCommentsUpdated(kysoCommentsUpdateEvent: KysoCommentsUpdateEvent) {}
+    async handleCommentsUpdated(event: KysoCommentsUpdateEvent) {
+        Logger.log(KysoEventEnum.COMMENTS_UPDATE, CommentsController.name)
+        Logger.debug(event, CommentsController.name)
+
+        const { frontendUrl, organization, team, report, user, comment} = event;
+
+        const centralizedMails: boolean = organization?.options?.notifications?.centralized || false
+        const emails: string[] = organization?.options?.notifications?.emails || []
+        const to = centralizedMails && emails.length > 0 ? emails : user.email
+
+        this.mailerService
+            .sendMail({
+                to,
+                subject: `Comment edited in report ${report.title}`,
+                template: 'comment-updated',
+                context: {
+                    frontendUrl,
+                    organization,
+                    team,
+                    report,
+                    comment
+                },
+            })
+            .then((messageInfo) => {
+                Logger.log(`Updated comment mail ${messageInfo.messageId} sent to ${Array.isArray(to) ? to.join(', ') : to}`, CommentsController.name)
+            })
+            .catch((err) => {
+                Logger.error(`An error occurred sending updated comment mail to ${Array.isArray(to) ? to.join(', ') : to}`, err, CommentsController.name)
+            })
+    }
 
     @EventPattern(KysoEventEnum.COMMENTS_DELETE)
-    async handleCommentsDeleted(kysoCommentsDeleteEvent: KysoCommentsDeleteEvent) {}
+    async handleCommentsDeleted(event: KysoCommentsDeleteEvent) {
+        Logger.log(KysoEventEnum.COMMENTS_DELETE, CommentsController.name)
+        Logger.debug(event, CommentsController.name)
+
+        const { frontendUrl, organization, team, report, user, comment} = event;
+
+        const centralizedMails: boolean = organization?.options?.notifications?.centralized || false
+        const emails: string[] = organization?.options?.notifications?.emails || []
+        const to = centralizedMails && emails.length > 0 ? emails : user.email
+
+        this.mailerService
+            .sendMail({
+                to,
+                subject: `Deleted a comment in report ${report.title}`,
+                template: 'comment-deleted',
+                context: {
+                    frontendUrl,
+                    organization,
+                    team,
+                    report,
+                    comment
+                },
+            })
+            .then((messageInfo) => {
+                Logger.log(`Deleted comment mail ${messageInfo.messageId} sent to ${Array.isArray(to) ? to.join(', ') : to}`, CommentsController.name)
+            })
+            .catch((err) => {
+                Logger.error(`An error occurred sending deleted comment mail to ${Array.isArray(to) ? to.join(', ') : to}`, err, CommentsController.name)
+            })
+    }
 }
