b4397639e3ba3655375588bb1a22eab6f7a62657
Fran Barrena <francisco@kyso.io>
2022-10-31 12:00:24 +0100

    feat: handling replies to comments

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -13,6 +13,38 @@ export class CommentsController {
         private readonly mailerService: MailerService
     ) {}
 
+    @EventPattern(KysoEventEnum.COMMENTS_REPLY)
+    async handleReplyToComment(event: KysoCommentsCreateEvent) {
+        Logger.log(KysoEventEnum.COMMENTS_REPLY, CommentsController.name)
+        Logger.debug(event, CommentsController.name)
+
+        const { frontendUrl, organization, team, report, user, comment} = event;
+
+        const centralizedMails: boolean = organization?.options?.notifications?.centralized || false
+        const emails: string[] = organization?.options?.notifications?.emails || []
+        const to = centralizedMails && emails.length > 0 ? emails : user.email
+
+        this.mailerService
+            .sendMail({
+                to,
+                subject: `New reply to your comment on report ${report.title}`,
+                template: 'comment-reply',
+                context: {
+                    frontendUrl,
+                    organization,
+                    team,
+                    report,
+                    comment
+                },
+            })
+            .then((messageInfo) => {
+                Logger.log(`New reply comment mail ${messageInfo.messageId} sent to ${Array.isArray(to) ? to.join(', ') : to}`, CommentsController.name)
+            })
+            .catch((err) => {
+                Logger.error(`An error occurred sending new reply comment mail to ${Array.isArray(to) ? to.join(', ') : to}`, err, CommentsController.name)
+            })
+    }
+
     @EventPattern(KysoEventEnum.COMMENTS_CREATE)
     async handleCommentsCreated(event: KysoCommentsCreateEvent) {
         Logger.log(KysoEventEnum.COMMENTS_CREATE, CommentsController.name)
