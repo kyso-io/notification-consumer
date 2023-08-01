703f79b8368b45a2ba23844a5aabca3b051e7ea3
Daniel Andújar <daniandujar@gmail.com>
2023-08-01 10:41:49 +0200

    fix: notify authors, owner and replier of the comment

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -16,19 +16,13 @@ export class CommentsController {
 
     private async sendMailReplyCommentInReport(kysoCommentsCreateEvent: KysoCommentsCreateEvent, email: string): Promise<void> {
         try {
-            const messageInfo: SentMessageInfo = await this.utilsService.sendHandlebarsEmail(
-                
-                email,
-                `New reply to your comment on report ${kysoCommentsCreateEvent.report.title}`,
-                'comment-reply',
-                {
-                    frontendUrl: kysoCommentsCreateEvent.frontendUrl,
-                    organization: kysoCommentsCreateEvent.organization,
-                    team: kysoCommentsCreateEvent.team,
-                    report: kysoCommentsCreateEvent.report,
-                    comment: kysoCommentsCreateEvent.comment,
-                }
-            )
+            const messageInfo: SentMessageInfo = await this.utilsService.sendHandlebarsEmail(email, `New reply on report ${kysoCommentsCreateEvent.report.title}`, 'comment-reply', {
+                frontendUrl: kysoCommentsCreateEvent.frontendUrl,
+                organization: kysoCommentsCreateEvent.organization,
+                team: kysoCommentsCreateEvent.team,
+                report: kysoCommentsCreateEvent.report,
+                comment: kysoCommentsCreateEvent.comment,
+            })
             Logger.log(`New reply comment mail ${messageInfo.messageId} sent to ${email}`, CommentsController.name)
         } catch (e) {
             Logger.error(`An error occurred sending new reply comment mail to ${email}`, e, CommentsController.name)
@@ -63,10 +57,28 @@ export class CommentsController {
                 }
             }
         } else {
-            const sendNotification: boolean = await this.utilsService.canUserReceiveNotification(user.id, 'replay_comment_in_report', organization.id, team.id)
-            if (sendNotification) {
-                if (report) {
-                    await this.sendMailReplyCommentInReport(kysoCommentsCreateEvent, parentCommentUser.email)
+            const users: User[] = [parentCommentUser]
+            if (parentCommentUser.id !== user.id) {
+                users.push(user)
+            }
+            if (Array.isArray(report.author_ids) && report.author_ids.length > 0) {
+                const authors: User[] = await this.db
+                    .collection<User>(Constants.DATABASE_COLLECTION_USER)
+                    .find({ id: { $in: report.author_ids } })
+                    .toArray()
+                for (const author of authors) {
+                    const index: number = users.findIndex((u: User) => u.id === author.id)
+                    if (index === -1) {
+                        users.push(author)
+                    }
+                }
+            }
+            for (const u of users) {
+                const sendNotification: boolean = await this.utilsService.canUserReceiveNotification(u.id, 'replay_comment_in_report', organization.id, team.id)
+                if (sendNotification) {
+                    if (report) {
+                        await this.sendMailReplyCommentInReport(kysoCommentsCreateEvent, u.email)
+                    }
                 }
             }
         }
@@ -74,20 +86,14 @@ export class CommentsController {
 
     private async sendMailNewCommentInReport(kysoCommentsCreateEvent: KysoCommentsCreateEvent, email: string): Promise<void> {
         try {
-            await this.utilsService.sendHandlebarsEmail(
-                
-                email,
-                `New comment in report ${kysoCommentsCreateEvent.report.title}`,
-                'comment-new',
-                {
-                    userCreatingAction: kysoCommentsCreateEvent.user,
-                    frontendUrl: kysoCommentsCreateEvent.frontendUrl,
-                    organization: kysoCommentsCreateEvent.organization,
-                    team: kysoCommentsCreateEvent.team,
-                    report: kysoCommentsCreateEvent.report,
-                    comment: kysoCommentsCreateEvent.comment,
-                }
-            );
+            await this.utilsService.sendHandlebarsEmail(email, `New comment in report ${kysoCommentsCreateEvent.report.title}`, 'comment-new', {
+                userCreatingAction: kysoCommentsCreateEvent.user,
+                frontendUrl: kysoCommentsCreateEvent.frontendUrl,
+                organization: kysoCommentsCreateEvent.organization,
+                team: kysoCommentsCreateEvent.team,
+                report: kysoCommentsCreateEvent.report,
+                comment: kysoCommentsCreateEvent.comment,
+            })
         } catch (e) {
             Logger.error(`An error occurred sending new comment mail to ${email}`, e, CommentsController.name)
         }
@@ -135,20 +141,14 @@ export class CommentsController {
 
     private async sendMailCommentUpdatedInReport(kysoCommentsUpdateEvent: KysoCommentsUpdateEvent, email: string): Promise<void> {
         try {
-            await this.utilsService.sendHandlebarsEmail(
-                
-                email,
-                `Comment edited in report ${kysoCommentsUpdateEvent.report.title}`,
-                'comment-updated',
-                {
-                    user: kysoCommentsUpdateEvent.user,
-                    frontendUrl: kysoCommentsUpdateEvent.frontendUrl,
-                    organization: kysoCommentsUpdateEvent.organization,
-                    team: kysoCommentsUpdateEvent.team,
-                    report: kysoCommentsUpdateEvent.report,
-                    comment: kysoCommentsUpdateEvent.comment,
-                },
-            )
+            await this.utilsService.sendHandlebarsEmail(email, `Comment edited in report ${kysoCommentsUpdateEvent.report.title}`, 'comment-updated', {
+                user: kysoCommentsUpdateEvent.user,
+                frontendUrl: kysoCommentsUpdateEvent.frontendUrl,
+                organization: kysoCommentsUpdateEvent.organization,
+                team: kysoCommentsUpdateEvent.team,
+                report: kysoCommentsUpdateEvent.report,
+                comment: kysoCommentsUpdateEvent.comment,
+            })
         } catch (e) {
             Logger.error(`An error occurred sending updated comment mail to ${email}`, e, CommentsController.name)
         }
@@ -196,20 +196,14 @@ export class CommentsController {
 
     private async sendMailDeleteCommentInReport(kysoCommentsDeleteEvent: KysoCommentsDeleteEvent, userReceiveEmail: User): Promise<void> {
         try {
-            await this.utilsService.sendHandlebarsEmail(
-                
-                userReceiveEmail.email,
-                `Deleted a comment in report ${kysoCommentsDeleteEvent.report.title}`,
-                'comment-deleted',
-                {
-                    user: kysoCommentsDeleteEvent.user,
-                    frontendUrl: kysoCommentsDeleteEvent.frontendUrl,
-                    organization: kysoCommentsDeleteEvent.organization,
-                    team: kysoCommentsDeleteEvent.team,
-                    report: kysoCommentsDeleteEvent.report,
-                    comment: kysoCommentsDeleteEvent.comment,
-                },
-            );
+            await this.utilsService.sendHandlebarsEmail(userReceiveEmail.email, `Deleted a comment in report ${kysoCommentsDeleteEvent.report.title}`, 'comment-deleted', {
+                user: kysoCommentsDeleteEvent.user,
+                frontendUrl: kysoCommentsDeleteEvent.frontendUrl,
+                organization: kysoCommentsDeleteEvent.organization,
+                team: kysoCommentsDeleteEvent.team,
+                report: kysoCommentsDeleteEvent.report,
+                comment: kysoCommentsDeleteEvent.comment,
+            })
         } catch (e) {
             Logger.error(`An error occurred sending deleted comment mail to ${userReceiveEmail.email}`, e, CommentsController.name)
         }
@@ -219,7 +213,7 @@ export class CommentsController {
     async handleCommentsDeleted(kysoCommentsDeleteEvent: KysoCommentsDeleteEvent) {
         Logger.log(KysoEventEnum.COMMENTS_DELETE, CommentsController.name)
         Logger.debug(kysoCommentsDeleteEvent, CommentsController.name)
-        const { organization, team, report, user } = kysoCommentsDeleteEvent
+        const { organization, team, report, user, comment } = kysoCommentsDeleteEvent
         const centralizedMails: boolean = organization?.options?.notifications?.centralized || false
         const emails: string[] = organization?.options?.notifications?.emails || []
         if (centralizedMails && Array.isArray(emails) && emails.length > 0) {
@@ -234,6 +228,16 @@ export class CommentsController {
             }
         } else {
             const users: User[] = [user]
+            const parentComment: Comment = await this.db.collection<Comment>(Constants.DATABASE_COLLECTION_COMMENT).findOne({ id: comment.comment_id })
+            if (parentComment) {
+                const parentCommentUser: User = await this.db.collection<User>(Constants.DATABASE_COLLECTION_USER).findOne({ id: parentComment.user_id })
+                if (parentCommentUser) {
+                    const index: number = users.findIndex((u: User) => u.id === parentCommentUser.id)
+                    if (index === -1) {
+                        users.push(parentCommentUser)
+                    }
+                }
+            }
             if (Array.isArray(report.author_ids) && report.author_ids.length > 0) {
                 const authors: User[] = await this.db
                     .collection<User>(Constants.DATABASE_COLLECTION_USER)
